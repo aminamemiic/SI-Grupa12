5240f6fc7b06d8b615837ba590aaa7e1f47608d8
faljic1 <faljic1@etf.unsa.ba>
2026-04-30 05:24:00 +0200

    fix/angular.json

--- PROJEKAT/FRONTEND/src/environments/environment.ts
+++ PROJEKAT/FRONTEND/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api' // Default to local for now, will be overridden in Railway
+  apiUrl: 'https://backend-production-1157.up.railway.app/api'
 };
